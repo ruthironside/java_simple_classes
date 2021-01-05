150c59e885047e6125b162262e2aea90d6229134
Ruth Ironside <ruthironside@live.com>
2021-01-05 17:14:26 +0000

    test water bottle has a volume

--- src/main/java/WaterBottle.java
+++ src/main/java/WaterBottle.java
@@ -1,2 +1,13 @@
 public class WaterBottle {
+    private float volume;
+
+
+    public WaterBottle(float volume) {
+        this.volume = volume;
+    }
+
+    public float getVolume() {
+        return this.volume;
+    }
+
 }
--- src/test/java/WaterBottleTest.java
+++ src/test/java/WaterBottleTest.java
@@ -1,2 +1,20 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class WaterBottleTest {
+
+    WaterBottle waterBottle;
+
+    @Before
+    public void setUp() {
+        waterBottle = new WaterBottle(100);
+    }
+
+    @Test
+    public void hasVolume() {
+        assertEquals(100, waterBottle.getVolume(), 0.01);
+    }
+
 }
